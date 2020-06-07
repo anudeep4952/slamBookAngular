b5688e5a519928111db72256bb8b42931617ed6f
bazigar <anudeep.insvirat@gmail.com>
2020-06-07 17:17:58 +0530

    cookie added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService ,SocialUser} from "angularx-social-login";
 import { GoogleLoginProvider } from "angularx-social-login";
 import{LoginServiceService} from '../../services/login-service.service'
+import{User} from '../../models/usermodel'
+import{ CookieService} from 'ngx-cookie-service'
+import {Router} from "@angular/router"
 
 @Component({
   selector: 'app-login',
@@ -11,8 +14,14 @@ import{LoginServiceService} from '../../services/login-service.service'
 export class LoginComponent implements OnInit {
 
    user: SocialUser;
+   validationObject:User;
+   viewPageUrl:string;
+   
 
-  constructor(private authService: AuthService,private loginService:LoginServiceService) { }
+  constructor(private authService: AuthService,
+    private loginService:LoginServiceService,
+    private cookieService:CookieService,
+    private router: Router)  { }
 
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
@@ -26,8 +35,26 @@ export class LoginComponent implements OnInit {
     this.authService.authState.subscribe((user) => {
       this.user = user;
       if(user){
-        //console.log(user);
-        this.loginService.verifyToken(user.idToken).subscribe((data)=>{console.log(data)})
+        this.loginService.verifyToken(user.idToken)
+        .subscribe((data)=>{
+        this.validationObject=data
+        this.cookieService.set(
+          'vcookie',JSON.stringify(this.validationObject),0.0416667,'/','localhost',false,"Strict"
+        )
+        this.validationObject=JSON.parse(this.cookieService.get('vcookie'))
+        console.log(this.validationObject)
+        this.viewPageUrl='/view/'+this.validationObject.userid
+        this.router.navigate([this.viewPageUrl])
+        },
+        (err)=>{
+          window.alert("no response from server")
+        })
+      }
+      else if(this.cookieService.check('vcookie')){
+        this.validationObject=JSON.parse(this.cookieService.get('vcookie'))
+        console.log(this.validationObject)
+        this.viewPageUrl='/view/'+this.validationObject.userid
+        this.router.navigate([this.viewPageUrl])
       }
     });
   
